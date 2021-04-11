ec33fb30432bcc0f16c970926f7dbd49467712e0
qbixmex <qbixmex@gmail.com>
2021-04-11 01:44:32 -0500

    Heroes App Creada

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,9 @@
+const HeroesApp = () => {
+  return (
+    <section>
+      <h1>Heroes App</h1>
+    </section>
+  );
+}
+
+export default HeroesApp;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import HeroesApp from './HeroesApp';
+import './index.css';
+
+ReactDOM.render(
+  <HeroesApp />,
+  document.getElementById('root')
+);
