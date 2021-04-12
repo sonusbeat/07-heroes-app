cd792aec45045c9a113dcebb8bb4c42ccd66dc39
qbixmex <qbixmex@gmail.com>
2021-04-11 21:28:14 -0500

    Leer argumentos del url

--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -1,5 +1,31 @@
+import { Redirect, useParams } from "react-router";
+import getHeroById from '../selectors/getHeroById';
+
 const HeroScreen = () => {
-  return ( <h2>Hero Screen</h2> );
+
+  const { heroId } = useParams();
+
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Redirect to="/" />
+  }
+
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
+
+  return (
+    <>
+      <h2>Hero Screen</h2>
+      <hr />
+      <pre>{ JSON.stringify(hero, null, 3) }</pre>
+    </>
+  );
 }
 
 export default HeroScreen;
--- src/routers/DashboardRoutes.jsx
+++ src/routers/DashboardRoutes.jsx
@@ -13,7 +13,7 @@ const DashboardRoutes = () => {
       <div className="container mt-4">
         <Switch>
           <Route exact path="/marvel" component={ MarvelScreen } />
-          <Route exact path="/heroe/:heroeId" component={ HeroScreen } />
+          <Route exact path="/hero/:heroId" component={ HeroScreen } />
           <Route exact path="/dc" component={ DcScreen } />
 
           <Redirect to="/marvel" />
