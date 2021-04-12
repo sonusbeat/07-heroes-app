692654fe41c529bf0309fa72be4edbff4ebd8532
qbixmex <qbixmex@gmail.com>
2021-04-11 21:55:03 -0500

    Hero Screen

--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -1,7 +1,7 @@
 import { Redirect, useParams } from "react-router";
 import getHeroById from '../selectors/getHeroById';
 
-const HeroScreen = () => {
+const HeroScreen = ({ history }) => {
 
   const { heroId } = useParams();
 
@@ -19,12 +19,43 @@ const HeroScreen = () => {
     characters,
   } = hero;
 
+  const HandleReturn = () => {
+    history.length <= 2 ? history.push("/") : history.goBack();
+  };
+
   return (
-    <>
-      <h2>Hero Screen</h2>
-      <hr />
-      <pre>{ JSON.stringify(hero, null, 3) }</pre>
-    </>
+    <section>
+      <div className="row mt-5">
+        <div className="col-4">
+          <img
+            src={ `../assets/${ heroId }.jpg` }
+            alt={ superhero }
+            className="img-thumbnail"
+          />
+        </div>{/* col */}
+
+        <div className="col-8">
+          <h2>{ superhero }</h2>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"><b>Alter Ego:</b>&nbsp;{ alter_ego }</li>
+            <li className="list-group-item"><b>Publisher:</b>&nbsp;{ publisher }</li>
+            <li className="list-group-item"><b>First Appearance:</b>&nbsp;{ first_appearance }</li>
+          </ul>
+
+          <h3>Characters</h3>
+          <p>{ characters }</p>
+
+          <button
+            className="btn btn-info"
+            onClick={ HandleReturn }
+          >
+            Return
+          </button>
+        </div>{/* col */}
+
+      </div>{/* row */}
+
+    </section>
   );
 }
 
