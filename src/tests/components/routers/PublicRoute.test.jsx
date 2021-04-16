2886f5576e6c758b5a6ad029292793f77950a701
qbixmex <qbixmex@gmail.com>
2021-04-15 22:53:31 -0500

    Public Route Test
    Debe de mostrar el componente

---
+++ src/tests/components/routers/PublicRoute.test.jsx
@@ -0,0 +1,32 @@
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import PublicRoute from '../../../routers/PublicRoute';
+
+describe('Pruebas en <PublicRoute />', () => {
+
+  const props = {
+    location: {
+      pathName: "/marvel"
+    }
+  };
+
+  test('Debe de mostrar el componente', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <PublicRoute
+          isAuthenticated={ false }
+          component={ () => <p> Ready! </p> }
+          { ...props }
+        />
+      </MemoryRouter>
+    );
+
+    const p = wrapper.find("p");
+
+    expect( p.exists() ).toBe(true);
+    expect( p.text().trim() ).toBe("Ready!");
+  });
+
+
+
+});
